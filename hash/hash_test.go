d9e05db4871c3aafd3791a3db0d21c452d73579c
JianFeng Ren (mynuolr) <3831242+mynuolr@users.noreply.github.com>
2020-12-08 22:09:41 +0800

    Add Hash

---
+++ hash/hash_test.go
@@ -0,0 +1,32 @@
+package hash
+
+import (
+	"testing"
+)
+
+func TestHash_Diff(t *testing.T) {
+	h1:=NewHash()
+	h1.Set("1","1")
+	h1.Set("2","2")
+	h1.Set("3","3")
+	h1.Set("4","4")
+	t.Log("RAW  :",h1)
+	h2:=NewHash()
+	h2.Set("2","2")
+	h2.Set("3","h")
+	t.Log("TOB  :",h2)
+	a,b:=h1.Diff(h2,func(i1 interface{},i2 interface{})bool {
+		if i1.(string)==i2.(string) {
+			return true
+		}
+		return false
+	})
+	if a.Len()!=3 {
+		t.Fail()
+	}
+	t.Log("lack :",a)
+	if b.Len()!=1 {
+		t.Fail()
+	}
+	t.Log("Added:",b)
+}
---
+++ hash/set.go
@@ -0,0 +1,76 @@
+package hash
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Check func(old interface{}, n interface{}) bool
+type Hash struct {
+	m map[string]interface{}
+	sync.RWMutex
+}
+
+func (h *Hash) Raw() map[string]interface{} {
+	h.RWMutex.Lock()
+	defer h.RWMutex.Unlock()
+	r := make(map[string]interface{})
+	for k, v := range h.m {
+		r[k] = v
+	}
+	return r
+}
+func (h *Hash) Len() int {
+	h.RWMutex.Lock()
+	defer h.RWMutex.Unlock()
+	return len(h.m)
+}
+func (h *Hash) Set(k string, i interface{}) {
+	h.RWMutex.Lock()
+	defer h.RWMutex.Unlock()
+	h.m[k] = i
+}
+func (h *Hash) Get(k string) interface{} {
+	h.RWMutex.RLock()
+	defer h.RWMutex.RUnlock()
+	if v, ok := h.m[k]; ok {
+		return v
+	} else {
+		return nil
+	}
+}
+func (h *Hash) Diff(hash *Hash, check ...Check) (lack, added *Hash) {
+	raw := h.Raw()
+	newHash := make(map[string]interface{})
+	for k, v := range hash.Raw() {
+		if v2, ok := raw[k]; ok {
+			if len(check) >= 0 {
+				for _, f := range check {
+					if !f(v2, v) {
+						goto B
+					}
+				}
+			}
+			delete(raw, k)
+			continue
+		}
+	B:
+		newHash[k] = v
+	}
+	lack = NewHash()
+	lack.m = raw
+	added = NewHash()
+	added.m = newHash
+	return
+}
+func (h *Hash)String()string  {
+	h.RWMutex.RLock()
+	defer h.RWMutex.RUnlock()
+	return fmt.Sprint(h.m)
+}
+func NewHash() *Hash {
+	return &Hash{
+		m:       map[string]interface{}{},
+		RWMutex: sync.RWMutex{},
+	}
+}
